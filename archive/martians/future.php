31052541320181493d52e5c049645120be56ba5e
Shaun Moss <shaun@astromultimedia.com>
2016-10-24 19:31:03 -0500

    Fixed link to celestia

--- archive/martians/future.php
+++ archive/martians/future.php
@@ -4,17 +4,21 @@ include("tpl/templateTop.php");
 
 <h2>Future Development</h2>
 
-<p>There is a program known as <a href='http://celestia.com'>Celestia</a> which enables the user to fly around the Solar System visiting whatever planets and moons they want.
+<p>There is a program known as <a href='http://celestiaproject.net/' target="_blank">Celestia</a> which
+  enables the user to fly around the Solar System visiting whatever planets and moons they want.
 
-<p>If we can model Mars, can we not then also model the Moon, and then perhaps even Earth?  Could we potentially model the whole Solar System as a game environment?  Although this seems very ambitious from this point, we might not think so in 5 or 10 years.  It would be extremely cool if we could do all the things from our Mars game anywhere in the Solar System.  By adding the kind of spaceflight that Celestia suggests (i.e. travel between worlds in a matter of seconds), players could operate all kinds of interesting businesses: shipping water from Earth to the Asteroids, mining Helium-3 on the Moon or in the atmosphere of Saturn, constructing bases on Pluto, mining platinum-group metals in the Asteroid Belt, etc., etc.
+<p>If we can model Mars, can we not then also model the Moon, and then perhaps even Earth? Could we
+  potentially model the whole Solar System as a game environment? Although this seems very ambitious
+  from this point, we might not think so in 5 or 10 years. It would be extremely cool if we could do
+  all the things from our Mars game anywhere in the Solar System. By adding the kind of spaceflight
+  that Celestia suggests (i.e. travel between worlds in a matter of seconds), players could operate
+  all kinds of interesting businesses: shipping water from Earth to the Asteroids, mining Helium-3
+  on the Moon or in the atmosphere of Saturn, constructing bases on Pluto, mining platinum-group
+  metals in the Asteroid Belt, etc., etc.
 
 <p>It would be cool.
 
-
-
 </p>
-
 
 <?php
 include("tpl/templateBottom.php");
-?>
