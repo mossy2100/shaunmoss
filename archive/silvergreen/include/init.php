30769032edc70a7b14b79ea98f48aa286adc28fc
Shaun Moss <shaun@astromultimedia.com>
2016-10-24 19:38:18 -0500

    Fixed paths in silvergreen

--- archive/silvergreen/include/init.php
+++ archive/silvergreen/include/init.php
@@ -6,44 +6,33 @@ session_start();
 
 // find base url and directory:
 $local = strpos($_SERVER['HTTP_HOST'], 'local') !== FALSE;
-if ($local)
-{
-  $smUrl = "http://local.shaunmoss.com";
-  $smDir = "/Users/shaun/Dropbox/PROJECTS/shaunmoss.com/shaunmoss6";
+if ($local) {
+  $smUrl = "http://shaunmoss.local";
+  $smDir = "/Users/shaun2/Dropbox/SITES/shaunmoss.com/htdocs";
 
-  $ascensionTekUrl = "$smUrl/archive/ascensiontek";
-  $ascensionTekDir = "$smDir/archive/ascensiontek";
-
-  $SilvergreenUrl = "$smUrl/archive/silvergreen";
-  $SilvergreenDir = "$smDir/archive/silvergreen";
-
-  $baseUrl = $SilvergreenUrl;
-  $baseDir = $SilvergreenDir;
-
-	$dbHost = "localhost";
-	$dbUsername = "shaun";
-	$dbPassword = "freedom";
-	$dbName = "ascensiontek";
+  $dbHost = "localhost";
+  $dbUsername = "shaun";
+  $dbPassword = "freedom";
+  $dbName = "ascensiontek";
 }
-else
-{
+else {
   $smUrl = "http://shaunmoss.com";
-  $smDir = "/var/www/shaunmoss.com";
+  $smDir = "/var/www/shaunmoss.com/htdocs";
 
-  $ascensionTekUrl = "$smUrl/archive/ascensiontek";
-  $ascensionTekDir = "$smDir/archive/ascensiontek";
-
-  $SilvergreenUrl = "$smUrl/archive/silvergreen";
-  $SilvergreenDir = "$smDir/archive/silvergreen";
-
-  $baseUrl = $SilvergreenUrl;
-  $baseDir = $SilvergreenDir;
-
-	$dbHost = "localhost";
-	$dbUsername = "ascen4_shaun";
-	$dbPassword = "freedom";
-	$dbName = "ascen4_ascensiontek";
+  $dbHost = "localhost";
+  $dbUsername = "ascen4_shaun";
+  $dbPassword = "freedom";
+  $dbName = "ascen4_ascensiontek";
 }
+
+$ascensionTekUrl = "$smUrl/archive/ascensiontek";
+$ascensionTekDir = "$smDir/archive/ascensiontek";
+
+$SilvergreenUrl = "$smUrl/archive/silvergreen";
+$SilvergreenDir = "$smDir/archive/silvergreen";
+
+$baseUrl = $SilvergreenUrl;
+$baseDir = $SilvergreenDir;
 
 $EcovillagesUrl = "$ascensionTekUrl/bizplan/pe";
 
